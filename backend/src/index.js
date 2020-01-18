db212199c367ab377c8d7e94d49f264f54ae639d
Bruno Cesar <bruunoocesar@gmail.com>
2020-01-17 21:31:38 -0300

    Extrai servidor http do express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const expressx = require('express')
 const mongoosx = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 const routes = require('./routes')
 
 const app = expressx()
+const server = http.Server(app)
 
 mongoosx.connect('mongodb+srv://batatinha:batatinha@cluster0-czshj.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -14,8 +16,4 @@ app.use(cors())
 app.use(expressx.json())
 app.use(routes)
 
-
-
-
-
-app.listen(3333)
+server.listen(3333)
