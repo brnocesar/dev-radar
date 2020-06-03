ebf11f605a5b8c0af239fe1f42ba0579747df237
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-03 09:18:15 -0300

    #12 - Adiciona create para 'points', sem upload de imagem

--- semana12_nlw1/server/src/routes.ts
+++ semana12_nlw1/server/src/routes.ts
@@ -13,12 +13,55 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/items/${item.image}`
+            id:         item.id,
+            title:      item.title,
+            image_url:  `http://localhost:3333/uploads/items/${item.image}`
         }
     });
     
     return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+
+    const {
+        name,           // const name = request.body.name
+        email,          // const email = request.body.email
+        whatsapp,       // (...)
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    // const trx = await knex.transaction();                // nao ta funcionando com isso
+
+    // const insertedIds = await trx('points').insert({
+    const insertedIds = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    }); // quando o nome da variável é igual ao do atributo do objeto, como em 'image', nao preciso atribuir
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: point_id,
+        };
+    });
+
+    // await trx('point_items').insert(pointItems);
+    await knex('point_items').insert(pointItems);
+
+    return response.json({ success: true});
 });
 
 export default routes;
