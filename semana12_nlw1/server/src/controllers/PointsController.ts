f50e2978bcf17468525614534054dbb91aa2c696
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-03 09:58:28 -0300

    #12 - Separa create de 'points' em controller proprio

---
+++ semana12_nlw1/server/src/controllers/PointsController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class PointsController {
+
+    async create(request: Request, response: Response) {
+
+        const {
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            items
+        } = request.body;
+
+        const trx = await knex.transaction();                           // nao ta funcionando com transaction
+
+        const point = {
+            image: 'image-fake',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf
+        };
+    
+        // const insertedIds = await trx('points').insert(point);
+        const insertedIds = await knex('points').insert(point);
+    
+        const point_id = insertedIds[0];
+    
+        const pointItems = items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id: point_id,
+            }
+        });
+    
+        // await trx('point_items').insert(pointItems);
+        await knex('point_items').insert(pointItems);
+
+        return response.json({
+            success: true,
+            id: point_id,
+            ...point,
+            items: items
+        });
+    }
+}
+
+export default PointsController;
--- semana12_nlw1/server/src/routes.ts
+++ semana12_nlw1/server/src/routes.ts
@@ -1,7 +1,11 @@
 import express, { request, response } from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/PointsController';
+
 const routes = express.Router();
+const pointsController = new PointsController();
+
 
 routes.get('/', (request, response) => {
 
@@ -22,46 +26,6 @@ routes.get('/items', async (request, response) => {
     return response.json(serializedItems);
 });
 
-routes.post('/points', async (request, response) => {
-
-    const {
-        name,           // const name = request.body.name
-        email,          // const email = request.body.email
-        whatsapp,       // (...)
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    // const trx = await knex.transaction();                // nao ta funcionando com isso
-
-    // const insertedIds = await trx('points').insert({
-    const insertedIds = await knex('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    }); // quando o nome da variável é igual ao do atributo do objeto, como em 'image', nao preciso atribuir
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id: point_id,
-        };
-    });
-
-    // await trx('point_items').insert(pointItems);
-    await knex('point_items').insert(pointItems);
-
-    return response.json({ success: true});
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
