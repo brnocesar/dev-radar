4d58a8c44d173122036ab654511e54f9dc904be1
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-04 20:34:09 -0300

    #12 - Adiciona e modifica estado no componente App

--- semana12_nlw1/web/src/App.tsx
+++ semana12_nlw1/web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState } from 'react';
 import './App.css';
 
 // JSX: Funcionalidade que permite escrever HTML (sintaxe de XML) dentro do JavaScript
@@ -6,11 +6,21 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+
+    const [counter, setCounter] = useState(0);
+
+    function clicador() {
+        setCounter(counter + 1)
+    }
+
     return (
         <div>
-            <Header title="Ecoleta" />
+            <Header title={`Contador: ${counter}`} />
             
-            <h1>Hello World!</h1>
+            <h1>{counter}</h1>
+
+            <button type="button" onClick={clicador}>Aumentar</button>
+            <button type="button" onClick={() => { alert('oi')}}>oi</button>
         </div>
     );
 }
