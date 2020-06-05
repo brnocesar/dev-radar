8a48fdd8a2fe4c518bf39584507b019222e71293
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-04 21:28:04 -0300

    #12 - Adiciona landing page

--- semana12_nlw1/web/src/App.tsx
+++ semana12_nlw1/web/src/App.tsx
@@ -1,27 +1,12 @@
-import React, {useState } from 'react';
+import React from 'react';
 import './App.css';
 
-// JSX: Funcionalidade que permite escrever HTML (sintaxe de XML) dentro do JavaScript
-
-import Header from './Header';
+import Home from './pages/Home'; // por padrao, ja busca pelo 'index.tsx'
 
 function App() {
 
-    const [counter, setCounter] = useState(0);
-
-    function clicador() {
-        setCounter(counter + 1)
-    }
-
     return (
-        <div>
-            <Header title={`Contador: ${counter}`} />
-            
-            <h1>{counter}</h1>
-
-            <button type="button" onClick={clicador}>Aumentar</button>
-            <button type="button" onClick={() => { alert('oi')}}>oi</button>
-        </div>
+        <Home />
     );
 }
 
