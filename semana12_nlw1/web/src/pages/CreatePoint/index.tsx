7c3ac67f9538ed89edc1e0ea029aa1558f303fc5
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-06 03:23:59 -0300

    #12 - Finaliza cadastro de ponto de coleta atraves do formulario

--- semana12_nlw1/web/src/pages/CreatePoint/index.tsx
+++ semana12_nlw1/web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -42,6 +42,7 @@ const CreatePoint = () => {
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>(initialPosition);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
+    const history = useHistory();
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -101,6 +102,33 @@ const CreatePoint = () => {
         setSelectedItems([ ...selectedItems, id]);
     }
 
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude , longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data = {
+            name, 
+            email, 
+            whatsapp, 
+            uf, 
+            city, 
+            latitude, 
+            longitude, 
+            items
+        };
+
+        await api.post('points', data);
+
+        alert('Ponto de coleta criado');
+
+        history.push('/');
+    }
+
 
     return (
         <div id="page-create-point">
@@ -113,7 +141,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do ponto de coleta</h1>
 
                 <fieldset>
