048c4e4469d9ddef64b4d3d6e354e0c43ccb50c6
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-07 15:33:08 -0300

    #12 - Controi tela de detalhes do ponto de coleta (Detail)

--- semana12_nlw1/mobile/src/pages/Detail/index.tsx
+++ semana12_nlw1/mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
-import { Feather as Icon } from '@expo/vector-icons';
+import { Feather as IconFE, FontAwesome as IconFA } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
 
@@ -15,21 +16,47 @@ const Detail = () => {
 
     return (
 
-        <View style={styles.container} >
-            <TouchableOpacity onPress={handleNavigateBack} >
-                <Icon name="arrow-left" size={30} color="#34cb79" />
-            </TouchableOpacity>
+        // <>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container} >
+                <TouchableOpacity onPress={handleNavigateBack} >
+                    <IconFE name="arrow-left" size={30} color="#34cb79" />
+                </TouchableOpacity>
 
-            <Text style={styles.title}>Tela de detalhes</Text>
-        </View>
+                <Image style={styles.pointImage} source={{ uri: "https://images.unsplash.com/photo-1553688737-e4fbcdad80c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+
+                <Text style={styles.pointName}>Ponto de coleta</Text>
+                <Text style={styles.pointItems}>Lâmpadas, Pilhas e baterias</Text>
+
+                <View style={styles.address}>
+                    <Text style={styles.addressTitle}>Endereço</Text>
+                    <Text style={styles.addressContent}>Curitiba, PR</Text>
+                </View>
+            </View>
+
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => {}}>
+                    <IconFA name="whatsapp" size={24} color="#fff" />
+                    <Text style={styles.buttonText}>WhatsApp</Text>
+                </RectButton>
+                <RectButton style={styles.button} onPress={() => {}}>
+                    <IconFE name="mail" size={24} color="#fff" />
+                    <Text style={styles.buttonText}>E-mail</Text>
+                </RectButton>
+            </View>
+        </SafeAreaView>
+        // </>
     );
 };
 
+
 const styles = StyleSheet.create({
+    
     container: {
         flex: 1,
-        paddingHorizontal: 32,
+        padding: 32,
         paddingTop: 20 + Constants.statusBarHeight,
+        // paddingTop: 20,
     },
 
     title: {
@@ -37,6 +64,73 @@ const styles = StyleSheet.create({
         fontFamily: 'Ubuntu_700Bold',
         marginTop: 24,
         color: '#322153',
+    },
+
+    pointImage: {
+        width: '100%',
+        height: 120,
+        resizeMode: 'cover',
+        borderRadius: 10,
+        marginTop: 32,
+    },
+
+    pointName: {
+        color: '#322153',
+        fontSize: 28,
+        fontFamily: 'Ubuntu_700Bold',
+        marginTop: 24,
+    },
+
+    pointItems: {
+        fontFamily: 'Roboto_400Regular',
+        fontSize: 16,
+        lineHeight: 24,
+        marginTop: 8,
+        color: '#6C6C80'
+    },
+
+    address: {
+        marginTop: 32,
+    },
+    
+    addressTitle: {
+        color: '#322153',
+        fontFamily: 'Roboto_500Medium',
+        fontSize: 16,
+    },
+
+    addressContent: {
+        fontFamily: 'Roboto_400Regular',
+        lineHeight: 24,
+        marginTop: 8,
+        color: '#6C6C80'
+    },
+
+    footer: {
+        borderTopWidth: StyleSheet.hairlineWidth,
+        borderColor: '#999',
+        paddingVertical: 20,
+        paddingHorizontal: 32,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // paddingBottom: 20,
+    },
+    
+    button: {
+        width: '48%',
+        backgroundColor: '#34CB79',
+        borderRadius: 10,
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        marginLeft: 8,
+        color: '#FFF',
+        fontSize: 16,
+        fontFamily: 'Roboto_500Medium',
     },
 });
 
