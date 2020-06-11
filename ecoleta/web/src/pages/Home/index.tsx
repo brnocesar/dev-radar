6894793367d88e887aa99841f4b3c9e021d29d4a
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-10 22:57:07 -0300

    #12 - Ajusta responsividade do bg e adiciona botao na home

--- ecoleta/web/src/pages/Home/index.tsx
+++ ecoleta/web/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiSearch } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
@@ -20,14 +20,24 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-                    <Link to="/cadastro">
-                        <span>
-                            <FiLogIn />
-                        </span>
-                        <strong>
-                            Cadastre um ponto de coleta
-                        </strong>
-                    </Link>
+                    <div className="buttons">
+                        <Link to="/pontos">
+                            <span>
+                                <FiSearch />
+                            </span>
+                            <strong>
+                                Encontre um ponto de coleta
+                            </strong>
+                        </Link>
+                        <Link to="/cadastro">
+                            <span>
+                                <FiLogIn />
+                            </span>
+                            <strong>
+                                Cadastre um ponto de coleta
+                            </strong>
+                        </Link>
+                    </div>
                 </main>
 
             </div>
