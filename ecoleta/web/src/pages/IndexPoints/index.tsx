2d6b46825f8ffd6de2ccdecfb3d31e1e9f058f0b
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-12 21:35:42 -0300

    #12 - Implementa filtro por estado e cidade na listagem de pontos

--- ecoleta/web/src/pages/IndexPoints/index.tsx
+++ ecoleta/web/src/pages/IndexPoints/index.tsx
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './styles.css';
 import NavHeader from '../../components/NavHeader';
 import api from '../../services/api';
 
+
+interface UF {
+    id: number;
+    sigla: string;
+    nome: string;
+};
+
+interface City {
+    id: number;
+    nome: string;
+};
 
 interface Point {
     id: number;
@@ -15,19 +28,54 @@ interface Point {
 
 const IndexPoint = () => {
 
+    const [ufs, setUfs] = useState<UF[]>([]);
+    const [cities, setCities] = useState<City[]>([]);
+    const [selectedUf, setSelectedUf] = useState('');
+    const [selectedCity, setSelectedCity] = useState('');
     const [points, setPoints] = useState<Point[]>([]);
 
+
     useEffect(() => {
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then(response => {
+            setUfs(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios?orderBy=nome`).then(response => {
+            setCities(response.data);
+        });
+    }, [selectedUf]);
+
+    useEffect(() => {
+        api.get('points').then(response => {
+            setPoints(response.data);
+        });
+    }, []);
+
+
+    function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value;
+
+        setSelectedUf(uf);
+
+        if ( uf === '' ) setSelectedCity('');
+    }
+
+    function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedCity(event.target.value);
+    }
+
+    function handleFilter() {
         api.get('points', {
             params: {
-                // city: 'Curitiba',
-                // uf: 'PA',
-                // items: "7,8,9",
+                uf: selectedUf,
+                city: selectedCity
             }
         }).then(response => {
             setPoints(response.data);
         });
-    }, []);
+    }
     
 
     return (
@@ -39,17 +87,58 @@ const IndexPoint = () => {
                 <main>
                     <h1>Pontos de coleta</h1>
 
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado</label>
+                            <select 
+                                name="uf" 
+                                id="uf" 
+                                value={selectedUf} 
+                                onChange={handleSelectedUf}
+                            >
+                                <option value="">Todos os Estados</option>
+                                {ufs.map(uf => (
+                                    <option key={uf.id} value={uf.sigla}>{uf.nome}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="uf">Cidade</label>
+                            <select 
+                                name="city" 
+                                id="city" 
+                                value={selectedCity} 
+                                onChange={handleSelectedCity}
+                                disabled={ !selectedUf }
+                            >
+                                <option value="">
+                                    { !selectedUf ? "Selecione um Estado" : "Selecione uma Cidade" }
+                                </option>
+                                {cities.map(city => (
+                                    <option key={city.id} value={city.nome}>{city.nome}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <button onClick={handleFilter}>
+                            Filtrar
+                        </button>
+                    </div>
+
                     <ul className="points-grid">
                         {points.map(point => (
                             <li 
                                 key={point.id}
                                 className="point-box"
                             >
-                                <img src={point.image_url} alt={point.name} />
+                                {/* <Link to={`/pontos/${point.id}`}> */}
+                                <Link to="/">
+                                    <img src={point.image_url} alt={point.name} />
+                                </Link>
                                 <h2>{point.name}</h2>
                                 <h3>{point.items}</h3>
-                                {/* <h3>Item 1, item 2, item 3</h3> */}
-                                <span>{point.city}, {point.uf}</span>
+                                <span>
+                                    {point.city}, {point.uf}
+                                </span>
                             </li>
                         ))}
                     </ul>
