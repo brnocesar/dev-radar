30135bd494d4c038fcf99625df48835741d7288b
Bruno Cesar <bruunoocesar@gmail.com>
2020-07-31 20:50:19 -0300

    Finaliza aula3

--- brnoflix/web/src/components/Footer/styles.js
+++ brnoflix/web/src/components/Footer/styles.js
@@ -6,8 +6,8 @@ export const FooterBase = styled.footer`
     border-top: 2px solid var(--brnoLaranja);
     padding-left: 16px;
     padding-right: 16px;
-    padding-top: 32px;
-    padding-bottom: 32px;
+    padding-top: 24px;
+    padding-bottom: 16px;
     color: var(--brnoLaranjaRosado);
     text-align: center;
     @media (max-width: 800px) {
--- brnoflix/web/src/pages/cadastro/Categoria/index.js
+++ brnoflix/web/src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import BaseTemplate from '../../../components/BaseTemplate';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+
+    const initialValues = {
+        name: '',
+        description: '',
+        color: '#cea69d'
+    };
+
+    const [categories, setCategories] = useState([]);
+    const [values, setValues] = useState(initialValues);
+
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value
+        });
+    }
+
+    function handleChange(event) {
+        // const { getAttribute, value } = event.target
+        setValue(
+            // getAttribute('name'), 
+            // value
+            event.target.getAttribute('name'), 
+            event.target.value
+        );
+    }
+
+
     return (
         <BaseTemplate>
 
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {values.name}</h1>
 
-            <form>
-                <label>
-                    Nome da Categoria:
-                    <input type="text" />
-                </label>
+            <form onSubmit={function handleSubmit(event) {
+                event.preventDefault();
+                setCategories([
+                    ...categories,
+                    values
+                ]);
+                setValues(initialValues);
+            }}>
+                <FormField 
+                    label="Nome da Categoria"
+                    type="text" 
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                />
+                
+                <div>
+                    <label>
+                        Descrição:
+                        <textarea 
+                            type="text" 
+                            name="description"
+                            value={values.description}
+                            onChange={handleChange}
+                            />
+                    </label>
+                </div>
+
+                <FormField 
+                    label="Cor"
+                    type="color" 
+                    name="color"
+                    value={values.color}
+                    onChange={handleChange}
+                />
 
                 <button>
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categories.map((category, index) => {
+                    return (
+                        <li key={`${category.name}${index}`}>
+                            {category.name}
+                        </li>
+                    );
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para Home
