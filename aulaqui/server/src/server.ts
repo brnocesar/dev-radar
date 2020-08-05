e9d57d23091cfe76f19bf0afc3e93e7c70ee4ab5
Bruno Cesar <bruunoocesar@gmail.com>
2020-08-04 22:55:15 -0300

    #aulaqui - Retira node-modules

--- aulaqui/server/src/server.ts
+++ aulaqui/server/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+    console.log('acessou a rota');
+    return response.json([{resposta: 'resposta'}]);
+});
+
+app.listen(3333);
