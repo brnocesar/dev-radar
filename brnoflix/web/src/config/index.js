372add9fc6cc921611da4a59554a1541fae4c529
Bruno Cesar <bruunoocesar@gmail.com>
2020-08-17 23:08:57 -0300

    Rearranja as pastas do dev-radar

--- brnoflix/web/src/config/index.js
+++
@@ -1,7 +0,0 @@
-const URL_BACKEND_API = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
-  : 'https://brnoflix.herokuapp.com';
-
-export default {
-  URL_BACKEND_API,
-};
